5b7e636205441e6201dc88b6a4c693e80f96d8c5
vaxiobbxx <66817172+vaxiobbxx@users.noreply.github.com>
2020-08-16 20:29:59 +0100

    Add files via upload

---
+++ index.lua
@@ -0,0 +1,44 @@
+local discordia = require('discordia')
+local client = discordia.Client()
+discordia.extensions()
+
+-- Turn on the bot
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+
+-- String Generation
+
+local ascii = {
+	{Min=48,Max=57},
+	{Min=65,Max=90},
+	{Min=97,Max=122},
+}
+local function genletter()
+	local bounds = ascii[math.random(3)]
+	return string.char(math.random(bounds.Min,bounds.Max))
+end
+local function gencode(length)
+	local code = ""
+	for i = 1, length do
+		code = code..genletter()
+	end
+	return code
+end
+
+-- Generation Command
+
+local numofcodes = 
+
+client:on('messageCreate', function(msg)
+	local args = string.split(msg.content," ")
+	if args[1] == "!gen" then
+		for i = 1, args[2] or 1 do
+			msg.channel:send("discord.gift/"..gencode(16))
+		end
+	end
+end)
+
+
+client:run('Bot NzQ0MjU3MDc5NTg5NzMyNDAy.Xzglaw.1CnIzHvmZx_2B2pYhgtgaoZWtO4')
